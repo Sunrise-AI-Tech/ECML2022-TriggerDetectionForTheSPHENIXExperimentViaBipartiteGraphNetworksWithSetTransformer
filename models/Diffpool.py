9eeb8f0f5227ba2c1c0b538e1d66ba58b86a2785
tingting <tingting.xuan@stonybrook.edu>
2021-06-16 11:01:31 -0400

    modify diffpool.py

--- models/Diffpool.py
+++ models/Diffpool.py
@@ -24,7 +24,7 @@ class Diffpool(nn.Module):
         self.name = 'Diffpool'
 
         #input model
-        self.input_network = make_mlp(3, [hidden_dim],
+        self.input_network = make_mlp(4, [hidden_dim],
                                       output_activation=hidden_activation,
                                       layer_norm=layer_norm)
 
@@ -41,13 +41,17 @@ class Diffpool(nn.Module):
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=lr_scheduler_decrease_rate)
         
                 
-    def forward(self, x, edge_index, batch, batch_size):
+    def forward(self, x, edge_index, batch, batch_size, e=None, is_e=False):
         node_output = self.input_network(x)
+        # print(node_output)
         n_hits = x.shape[0] // batch_size
         nodes = node_output.view(batch_size.item(), n_hits, node_output.shape[1])
         A = torch.cuda.FloatTensor(batch_size, n_hits, n_hits).fill_(0)
         start, end = edge_index
-        A[batch[start], (start-n_hits*batch[start]), (end-n_hits*batch[start])] = 1
+        if is_e:
+            A[batch[start], (start-n_hits*batch[start]), (end-n_hits*batch[start])] = e.reshape(-1)
+        else:
+            A[batch[start], (start-n_hits*batch[start]), (end-n_hits*batch[start])] = 1
         ip_pred = self.ip_pred_diffpool(nodes, A, batch_num_nodes=None)
         return ip_pred
 
